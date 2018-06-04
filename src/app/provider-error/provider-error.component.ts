1fec150f1206e5e5f5878d9f09c09ce136cb7bb8
Ryan Noble <ryanjnoble@gmail.com>
2018-06-04 18:49:55 +0200

    Commiting for safety
    Laptop complaining, preserving code

--- src/app/provider-error/provider-error.component.ts
+++ src/app/provider-error/provider-error.component.ts
@@ -1,3 +1,4 @@
+import { Web3Service } from './../shared/web3.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./provider-error.component.scss']
 })
 export class ProviderErrorComponent implements OnInit {
+  public errorMessage: string;
 
-  constructor() { }
+  constructor(private web3: Web3Service) { }
 
   ngOnInit() {
+    this.checkWeb3();
   }
 
+  checkWeb3() {
+    console.log(this.web3.accountsObservable);
+    if (!this.web3.ready) {
+      this.errorMessage = 'Please install MetaMask or Cypherbrowser';
+    }
+    // setTimeout(this.checkWeb3(), 2000);
+  }
 }
