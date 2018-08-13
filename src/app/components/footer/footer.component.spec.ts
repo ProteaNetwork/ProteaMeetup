4e30c3c1eb8542790623ecf98e7ee89dce7dca17
Ryan Noble <ryanjnoble@gmail.com>
2018-08-14 00:30:38 +0200

    Spelling error

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 
-describe('HeaderComponent', () => {
+describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
